8be119a53e64196ec1168f294cb2e7f07d4c6c70
Vincent Vidal <vincent@vidal.pm>
2025-12-04 09:26:30 +0100

    sync vendor

---
+++ Android.mk
@@ -0,0 +1,24 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),brax3)
+
+$(call add-radio-file-sha1-checked,radio/connsys_bt.img,bd85d8428ff0e32b3f539672cbfc9a107de8ff02)
+$(call add-radio-file-sha1-checked,radio/dpm.img,f1b20d254f5974e3609655a51f8a37db74e16f04)
+$(call add-radio-file-sha1-checked,radio/gz.img,bd0b30bf40b01ccd5511eef2bfbd0a97ae2191d2)
+$(call add-radio-file-sha1-checked,radio/lk.img,781c7dc9795632df2490d14516faa9c517938a90)
+$(call add-radio-file-sha1-checked,radio/logo.img,23b99b242ec8a363b91e71c45247af397f31893e)
+$(call add-radio-file-sha1-checked,radio/mcf_ota.img,108b6d9ebfb324635e786ac940a6d714db7b1bc5)
+$(call add-radio-file-sha1-checked,radio/mcupm.img,f5e6ae334e48a931e0b41a381e4e6bed3490283f)
+$(call add-radio-file-sha1-checked,radio/md1img.img,fd208c22a2293423acfd337a14ab87f9601edd33)
+$(call add-radio-file-sha1-checked,radio/pi_img.img,88edb046e702077597ab7f89894b25f0b35575b6)
+$(call add-radio-file-sha1-checked,radio/scp.img,ecb102730ab05feda2bcedce34f8c1777203a6fc)
+$(call add-radio-file-sha1-checked,radio/spmfw.img,e163e831255c352f0b7f68ad1f0d2c5d8ca7aa0e)
+$(call add-radio-file-sha1-checked,radio/sspm.img,816ebaf635bb19e032afedb23a1af6aa78256b59)
+$(call add-radio-file-sha1-checked,radio/tee.img,5b6b247be40763a0dbf236471cebc63257eef7e8)
+$(call add-radio-file-sha1-checked,radio/vcp.img,439849405e481ebdb80935c5d7c1557cfacf1eb3)
+
+endif
---
+++ BoardConfigVendor.mk
@@ -0,0 +1,19 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+AB_OTA_PARTITIONS += \
+    connsys_bt \
+    dpm \
+    gz \
+    lk \
+    logo \
+    mcf_ota \
+    mcupm \
+    md1img \
+    pi_img \
+    scp \
+    spmfw \
+    sspm \
+    tee \
+    vcp
